49f6ba1a44bc12bdb940ccb5373d22b0b470cafd
Andrew Kessler <andrewksslr@gmail.com>
2026-06-01 21:30:28 +0200

    RMB mouse look restored

--- src/engine/app.rs
+++ src/engine/app.rs
@@ -15,8 +15,10 @@ use pixels::{
 use winit::{
     dpi::LogicalSize,
     event::{
+        DeviceEvent,
         ElementState,
         Event,
+        MouseButton,
         WindowEvent,
     },
     event_loop::EventLoop,
@@ -113,6 +115,9 @@ let menu_background =
 
         let mut menu_index =
             0usize;
+        
+        let mut right_mouse =
+            false;
 
         let event_loop =
             EventLoop::new()
@@ -152,187 +157,251 @@ let menu_background =
             .unwrap();
 
         event_loop
-            .run(
-                move |event, target| {
+    .run(
+        move |event, target| {
 
-                    match event {
+            match event {
 
-                        Event::WindowEvent {
+                Event::DeviceEvent {
 
-                            event:
-                                WindowEvent::CloseRequested,
+                    event:
+                        DeviceEvent::MouseMotion {
+
+                            delta,
+                        },
+
+                    ..
+                } => {
+
+                    if game_state
+                        ==
+                        GameState::Playing
+                        &&
+                        right_mouse
+                    {
+
+                        player.angle +=
+                            delta.0 as f32
+                                * 0.003;
+
+                        player.pitch +=
+                            delta.1 as f32
+                                * 0.50;
+
+                        player.pitch =
+                            player.pitch.clamp(
+                                -80.0,
+                                80.0,
+                            );
+                    }
+                }
+
+                Event::WindowEvent {
+
+                    event:
+                        WindowEvent::CloseRequested,
+
+                    ..
+                } => {
+
+                    target.exit();
+                }
+
+                Event::WindowEvent {
+
+                    event:
+                        WindowEvent::MouseInput {
+
+                            state,
+
+                            button,
 
                             ..
-                        } => {
+                        },
 
-                            target.exit();
-                        }
+                    ..
+                } => {
 
-                        Event::WindowEvent {
+                    if button
+                        ==
+                        MouseButton::Right
+                    {
 
-                            event:
-                                WindowEvent::KeyboardInput {
-
-                                    event,
-
-                                    ..
-                                },
-
-                            ..
-                        } => {
-
-                            if let PhysicalKey::Code(
-                                keycode
-                            ) =
-                                event.physical_key
-                            {
-
-                                match event.state {
-
-                                    ElementState::Pressed => {
-
-                                        keys.insert(
-                                            keycode
-                                        );
-
-                                        if game_state
-                                        ==
-                                        GameState::Menu
-                                        {
-
-                                            match keycode {
-
-                                                KeyCode::ArrowUp => {
-
-                                                    if menu_index > 0 {
-
-                                                        menu_index -= 1;
-                                                    }
-                                                }
-
-                                                KeyCode::ArrowDown => {
-
-                                                    if menu_index < 3 {
-
-                                                        menu_index += 1;
-                                                    }
-                                                }
-
-                                                KeyCode::Enter => {
-
-                                                    match menu_index {
-
-                                                        0 => {
-
-                                                            println!(
-                                                                "Starting game..."
-                                                            );
-
-                                                            game_state =
-                                                                GameState::Playing;
-                                                        }
-
-                                                        1 => {
-
-                                                            println!(
-                                                                "Save Game placeholder"
-                                                            );
-                                                        }
-
-                                                        2 => {
-
-                                                            println!(
-                                                                "Load Game placeholder"
-                                                            );
-                                                        }
-
-                                                        3 => {
-
-                                                            game_state =
-                                                                GameState::Exit;
-                                                        }
-
-                                                        _ => {}
-                                                    }
-                                                }
-
-        _ => {}
-    }
-}
-
-else {
-
-    match keycode {
-
-        KeyCode::Escape => {
-
-            game_state =
-                GameState::Menu;
-        }
-
-        _ => {}
-    }
-}
-                                    }
-
-                                    ElementState::Released => {
-
-                                        keys.remove(
-                                            &keycode
-                                        );
-                                    }
-                                }
-                            }
-                        }
-
-                        Event::AboutToWait => {
-
-                            if game_state
+                        right_mouse =
+                            state
                                 ==
-                                GameState::Playing
-                            {
+                                ElementState::Pressed;
+                    }
+                }
 
-                                update_player(
-                                    &mut player,
-                                    &keys,
-                                    &map,
+                Event::WindowEvent {
+
+                    event:
+                        WindowEvent::KeyboardInput {
+
+                            event,
+
+                            ..
+                        },
+
+                    ..
+                } => {
+
+                    if let PhysicalKey::Code(
+                        keycode
+                    ) =
+                        event.physical_key
+                    {
+
+                        match event.state {
+
+                            ElementState::Pressed => {
+
+                                keys.insert(
+                                    keycode
                                 );
 
-                                if keys.contains(
-                                    &KeyCode::ArrowLeft
-                                ) {
+                                if game_state
+                                    ==
+                                    GameState::Menu
+                                {
 
-                                    player.angle -=
-                                        0.05;
+                                    match keycode {
+
+                                        KeyCode::ArrowUp => {
+
+                                            if menu_index > 0 {
+
+                                                menu_index -= 1;
+                                            }
+                                        }
+
+                                        KeyCode::ArrowDown => {
+
+                                            if menu_index < 3 {
+
+                                                menu_index += 1;
+                                            }
+                                        }
+
+                                        KeyCode::Enter => {
+
+                                            match menu_index {
+
+                                                0 => {
+
+                                                    println!(
+                                                        "Starting game..."
+                                                    );
+
+                                                    game_state =
+                                                        GameState::Playing;
+                                                }
+
+                                                1 => {
+
+                                                    println!(
+                                                        "Save Game placeholder"
+                                                    );
+                                                }
+
+                                                2 => {
+
+                                                    println!(
+                                                        "Load Game placeholder"
+                                                    );
+                                                }
+
+                                                3 => {
+
+                                                    game_state =
+                                                        GameState::Exit;
+                                                }
+
+                                                _ => {}
+                                            }
+                                        }
+
+                                        _ => {}
+                                    }
                                 }
 
-                                if keys.contains(
-                                    &KeyCode::ArrowRight
-                                ) {
+                                else {
 
-                                    player.angle +=
-                                        0.05;
+                                    match keycode {
+
+                                        KeyCode::Escape => {
+
+                                            game_state =
+                                                GameState::Menu;
+                                        }
+
+                                        _ => {}
+                                    }
                                 }
                             }
 
-                            window
-                                .request_redraw();
+                            ElementState::Released => {
+
+                                keys.remove(
+                                    &keycode
+                                );
+                            }
+                        }
+                    }
+                }
+
+                Event::AboutToWait => {
+
+                    if game_state
+                        ==
+                        GameState::Playing
+                    {
+
+                        update_player(
+                            &mut player,
+                            &keys,
+                            &map,
+                        );
+
+                        // Keep arrow turning
+                        // until mouse look is verified
+
+                        if keys.contains(
+                            &KeyCode::ArrowLeft
+                        ) {
+
+                            player.angle -=
+                                0.05;
                         }
 
-                        Event::WindowEvent {
+                        if keys.contains(
+                            &KeyCode::ArrowRight
+                        ) {
 
-                            event:
-                                WindowEvent::RedrawRequested,
+                            player.angle +=
+                                0.05;
+                        }
+                    }
 
-                            ..
-                        } => {
+                    window
+                        .request_redraw();
+                }
 
-                            let frame =
-                                pixels.frame_mut();
+                Event::WindowEvent {
 
-                            match game_state {
+                    event:
+                        WindowEvent::RedrawRequested,
 
-                                GameState::Menu => {
+                    ..
+                } => {
+
+                    let frame =
+                        pixels.frame_mut();
+
+                    match game_state {
+
+                        GameState::Menu => {
 
                             render_menu(
                                 frame,
@@ -343,31 +412,31 @@ else {
                             );
                         }
 
-                                GameState::Playing => {
+                        GameState::Playing => {
 
-                                    render_world(
-                                        frame,
-                                        &player,
-                                        &map,
-                                        &textures.textures,
-                                    );
-                                }
-
-                                GameState::Exit => {
-
-                                    target.exit();
-                                }
-                            }
-
-                            pixels
-                                .render()
-                                .unwrap();
+                            render_world(
+                                frame,
+                                &player,
+                                &map,
+                                &textures.textures,
+                            );
                         }
 
-                        _ => {}
+                        GameState::Exit => {
+
+                            target.exit();
+                        }
                     }
+
+                    pixels
+                        .render()
+                        .unwrap();
                 }
-            )
-            .unwrap();
+
+                _ => {}
+            }
+        }
+    )
+    .unwrap();
     }
 }
